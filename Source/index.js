5cdbe507f3f50370543bd6780393468679a20b9a
Naofumi-Alu <joseabrahammaciastorres@gmail.com>
2024-02-12 07:17:04 -0500

    update package.json to Engine version node to 18.x AND ADJUST

--- Source/index.js
+++ Source/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app= express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const portEnv = process.env.PORT || 3000;
+const portEnv = process.env.PORT; //|| 3000;
 
 //Settings
 app.set('port',portEnv);

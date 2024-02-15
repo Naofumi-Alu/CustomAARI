74d30f0663dd17b415b40c4bec92078d5e293278
Naofumi-Alu <joseabrahammaciastorres@gmail.com>
2024-02-15 10:25:35 -0500

    fix manaeData.js

--- Source/controller/manageData.js
+++ Source/controller/manageData.js
@@ -4,7 +4,6 @@ let generalData = [];
    // Aquí se implementaría la lógica para almacenar los datos
     // Por ahora, solo simularé que se almacenan y devuelvo un objeto
     // customiza mi objeto json de forma que tenga una propiedad principal data={}.
-    data = {data: data};
     generalData.push(data);
     console.log('Almacenando datos:', data);
     return data;
